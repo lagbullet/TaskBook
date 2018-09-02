94cae78001a7f482151aa01835ef81fc339de94e
lagbullet <40629577+lagbullet@users.noreply.github.com>
2018-09-02 21:28:00 +0300

    1

---
+++ BookApp/Book.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Web.Script.Serialization;
+
+namespace BookApp
+{
+    public class Book
+    {
+        private string _Name;
+        private string _Author;
+        private string _ReleaseDate;
+        
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string ReleaseDate { get; set; }
+
+        public Book() { }
+        public Book(string name, string author, string releasedate)
+        {
+            Name = name;
+            Author = author;
+            ReleaseDate = releasedate;
+        }
+        public string ToXML()
+        {
+            StringWriter sw = new StringWriter();
+            XmlTextWriter tw = null;
+            XmlSerializer serializer = new XmlSerializer(GetType());
+            tw = new XmlTextWriter(sw);
+            serializer.Serialize(tw, this);
+            return sw.ToString();
+        }
+
+        public static Book FromXML(string xmlText)
+        {
+            var stringReader = new StringReader(xmlText);
+            var serializer = new XmlSerializer(typeof(Book));
+            return serializer.Deserialize(stringReader) as Book;
+        }
+
+        public string ToJSON()
+        {
+            return new JavaScriptSerializer().Serialize(this);
+        }
+
+        public static Book FromJSON(string jsondata)
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Book blogObject = js.Deserialize<Book>(jsondata);
+            return blogObject;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Book Name - {0}, Author - {1}, Release Date - {2}",Name,Author,ReleaseDate);
+        }
+
+    }
+}
---
+++ BookApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BookApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"E:\Test\TestJson.txt");
+            Book book = Book.FromJSON(text);
+            book.Show();
+            Console.ReadLine();
+            text = System.IO.File.ReadAllText(@"E:\Test\TestXml.txt");
+            Book book1 = Book.FromXML(text);
+            book1.Show();
+            Console.ReadLine();
+            Console.WriteLine(book.ToJSON());
+            Console.ReadLine();
+            Console.WriteLine(book1.ToXML());
+            Console.ReadLine();
+        }
+    }
+}
